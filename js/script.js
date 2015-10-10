920040b5963d4d5c57d4edb4f9ae77f4e096581d
Danko-M <dankomilutinovic@gmail.com>
2015-10-10 22:36:35 +0200

    3th commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(function(){ 
-	//jQuery code here 
+
+	// gradien text
+	if ($(window).width() > 400) {
+   	$(".gradient-01").pxgradient({ 
+		  step: 20, 
+		  colors: ["#e9374b","#fd6020"], 
+		  dir: "x" 
+		});
+   }
+
 });
