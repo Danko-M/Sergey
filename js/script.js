5dcc059efcebec1fdcf6d00e7d7276df3e1529ac
Danko-M <dankomilutinovic@gmail.com>
2015-10-21 19:20:58 +0200

    added preloader for two  images on main page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
+
+/* 
+Author: Danko Milutinovic
+Desctiption: Stylesheet for the ШКОЛА ИГРОВОЙ ЛОГОПЕДИИ
+Contact:  
+        http://danko-m.github.io/ ,
+        dankomilutinovic@gmail.com, 
+        https://rs.linkedin.com/pub/danko-milutinovic/a3/675/778         
+*/
+
 $(function(){ 
+
+	// preload images for hover effect on main page
+	$.preloadImages = function() {
+	  for (var i = 0; i < arguments.length; i++) {
+	    $("<img />").attr("src", arguments[i]);
+	  }
+	};
+
+	$.preloadImages("img/video-player-mask-hover.jpg","img/inst-02-hover.jpg");
 
 
 });
